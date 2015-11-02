da6e6ad7ab0a1ad64ac11e647c8b9bb0a0a91d23
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-02 13:37:20 +0200

    Remove tabindex.

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -12,7 +12,7 @@
 		<span class="screen-reader-text"><?php esc_html_e( 'Sidebar', 'munsa' ); ?></span>
 	</button>
 
-	<aside id="sidebar-primary" class="sidebar-primary sidebar animated" role="complementary" tabindex="0" <?php hybrid_attr( 'sidebar', 'primary' ); ?>>
+	<aside id="sidebar-primary" class="sidebar-primary sidebar animated" role="complementary" <?php hybrid_attr( 'sidebar', 'primary' ); ?>>
 		<h2 class="screen-reader-text" id="sidebar-primary-header"><?php echo esc_html_x( 'Primary Sidebar', 'Sidebar aria label', 'munsa' ); ?></h2>
 		
 		<button class="sidebar-primary-toggle sidebar-primary-close menu-sidebar-close menu-sidebar-toggle" aria-controls="sidebar-primary">
