247e30143e148d59f1e04ccfe501e60ff230b2f0
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-02 13:27:44 +0200

    Remove data attributes. They are not needed becaue I decided not to use bootstrap modal.

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -8,14 +8,14 @@
 
 <?php if ( is_active_sidebar( 'primary' ) ) : // If the sidebar has widgets. ?>
 
-	<button class="sidebar-primary-toggle menu-sidebar-toggle" aria-controls="sidebar-primary" data-toggle="modal" data-backdrop="false" data-target="#sidebar-primary">
+	<button class="sidebar-primary-toggle menu-sidebar-toggle" aria-controls="sidebar-primary">
 		<span class="screen-reader-text"><?php esc_html_e( 'Info', 'munsa' ); ?></span>
 	</button>
 
 	<aside id="sidebar-primary" class="sidebar-primary sidebar animated" role="complementary" tabindex="0" <?php hybrid_attr( 'sidebar', 'primary' ); ?>>
 		<h2 class="screen-reader-text" id="sidebar-primary-header"><?php echo esc_html_x( 'Primary Sidebar', 'Sidebar aria label', 'munsa' ); ?></h2>
 		
-		<button class="sidebar-primary-toggle sidebar-primary-close menu-sidebar-close menu-sidebar-toggle" data-dismiss="modal" aria-controls="sidebar-primary">
+		<button class="sidebar-primary-toggle sidebar-primary-close menu-sidebar-close menu-sidebar-toggle" aria-controls="sidebar-primary">
 			<span class="screen-reader-text"><?php esc_html_e( 'Close Sidebar', 'munsa' ); ?></span>
 		</button>
 		
