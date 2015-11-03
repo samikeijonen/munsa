891f5b11ec8e9db1182e29ea602cee4d799f898d
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-03 13:42:02 +0200

    Change esc_html_x to esc_attr_x like in Core.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for displaying search forms in Twenty Sixteen
+ * Template for displaying search forms.
  *
  * @package Munsa
  */
@@ -8,8 +8,8 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'munsa' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_html_x( 'Search &hellip;', 'placeholder', 'munsa' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'munsa' ); ?>" />
+		<span class="screen-reader-text"><?php echo esc_attr_x( 'Search for:', 'label', 'munsa' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'munsa' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'munsa' ); ?>" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'munsa' ); ?></span></button>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo esc_attr_x( 'Search', 'submit button', 'munsa' ); ?></span></button>
 </form>
