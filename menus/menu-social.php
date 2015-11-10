627c6269822634f91dcf0876cd05f3537661b90c
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-11-10 10:25:28 +0200

    Take ID off because we can have the social menu in two different locations.

--- menus/menu-social.php
+++ menus/menu-social.php
@@ -8,7 +8,7 @@
 
 <?php if ( has_nav_menu( 'social' ) ) : // Check if there's a menu assigned to the 'social' location. ?>
 	
-	<nav id="menu-social" class="menu-social social-navigation menu" role="navigation" aria-label="<?php esc_attr_e( 'Social Menu', 'munsa' ); ?>" <?php hybrid_attr( 'menu', 'social' ); ?>>
+	<nav class="menu-social social-navigation menu" role="navigation" aria-label="<?php esc_attr_e( 'Social Menu', 'munsa' ); ?>" <?php hybrid_attr( 'menu', 'social' ); ?>>
 		<h2 class="screen-reader-text"><?php esc_attr_e( 'Social Menu', 'munsa' ); ?></h2>
 		
 		<?php wp_nav_menu(
@@ -20,6 +20,6 @@
 				'fallback_cb'    => '',
 			)
 		); ?>
-	</nav><!-- #menu-social -->
+	</nav><!-- .menu-social -->
 
 <?php endif; // End check for menu. ?>
